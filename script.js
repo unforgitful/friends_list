e80e40d6fefe92a5d0feac19f9e2fca44de2fa84
unforgitful <dunkfunker@gmail.com>
2024-11-26 17:58:06 +0800

    search function changes

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function userFunction() {
     for (i = 0; i < li.length; i++) {
         h2 = li[i].getElementsByTagName("h2")[0];
         textValue = h2.textContent || h2.innerText;
-        if (textValue.toUpperCase().indexOf(filter) > -1) {
+        if (textValue.toUpperCase().indexOf(filter) === 0) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
@@ -19,8 +19,27 @@ function userFunction() {
 }
 
 //SORTING FUNCTION
+//need fixing
 
 // function listSort() {
-//     let list, i, sort, b, unsort;
-//     list = document.getElementById
+//     let list, i, sort, b, unSort;
+//     list = document.getElementById("friendData");
+//     sort = true;
+
+//     while (sort) {
+//         sort = false;
+//         b = list.getElementsByTagName("h2");
+//             for (i = 0; i < (b.length - 1); i++) {
+//                 unSort = false;
+
+//                 if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+//                     unSort = true;
+//                     break;
+//                 }              
+//             }
+//             if (unSort) {
+//                 b[i].parentNode.insertBefore(b[i + 1], b[i]);
+//                 sort = true;
+//             }
+//     }
 // }
